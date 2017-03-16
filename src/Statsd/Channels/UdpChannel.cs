5c85d4e107b1f12c7b61c5edfd254ee2a1f04c35
Jury Soldatenkov <yurysold@gmail.com>
2017-03-16 12:11:52 +0300

    Created more informative exception and allow to ignore them

--- src/Statsd/Channels/UdpChannel.cs
+++ src/Statsd/Channels/UdpChannel.cs
@@ -8,7 +8,24 @@ namespace Codestellation.Statsd.Channels
     /// </summary>
     public class UdpChannel : IChannel, IDisposable
     {
+        private readonly UdpChannelSettings _settings;
         private readonly UdpClient _updClient;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="UdpChannel"/> class
+        /// </summary>
+        /// <param name="settings">Udp channel settings</param>
+        public UdpChannel(UdpChannelSettings settings)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+            _settings = settings;
+
+            _updClient = new UdpClient();
+            TryConnect();
+        }
 
         /// <summary>
         /// Creates a new instance of <see cref="UdpChannel"/> class
@@ -16,19 +33,8 @@ namespace Codestellation.Statsd.Channels
         /// <param name="hostname">Statsd server host. Must be either valid ip address or dns name</param>
         /// <param name="port">Port a statsd server listens to</param>
         public UdpChannel(string hostname, int port)
+            : this(new UdpChannelSettings { Host = hostname, Port = port })
         {
-            if (string.IsNullOrWhiteSpace(hostname))
-            {
-                throw new ArgumentException($"Expected a valid host name, but received '{hostname}'", nameof(hostname));
-            }
-
-            if (port <= 0 || UInt16.MaxValue < port)
-            {
-                throw new ArgumentOutOfRangeException(nameof(port), $"Expected a number between 1 and {UInt16.MaxValue} but received {port}");
-            }
-
-            _updClient = new UdpClient();
-            _updClient.Client.Connect(hostname, port);
         }
 
         /// <inheritdoc />
@@ -43,14 +49,37 @@ namespace Codestellation.Statsd.Channels
             {
                 return;
             }
-
-            _updClient.Client.Send(buffer, count, SocketFlags.None);
+            if (!_updClient.Client.Connected)
+            {
+                TryConnect();
+            }
+            if (_updClient.Client.Connected)
+            {
+                _updClient.Client.Send(buffer, count, SocketFlags.None);
+            }
         }
 
         /// <inheritdoc />
         public void Dispose()
         {
             _updClient?.Dispose();
+        }
+
+        private void TryConnect()
+        {
+            try
+            {
+                _updClient.Client.Connect(_settings.Host, _settings.Port);
+            }
+            catch (SocketException e)
+            {
+                var message = $"UdpChannel was not able to send data to 'udp://{_settings.Host}:{_settings.Port}'. " +
+                    "Possible reasons are invalid IP address or unresolvable DNS name.";
+                if (_settings.IgnoreDnsErrors)
+                {
+                    throw new InvalidOperationException(message, e);
+                }
+            }
         }
     }
 }
---
+++ src/Statsd/Channels/UdpChannelSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Codestellation.Statsd.Channels
+{
+    /// <summary>
+    /// Contains specific settings for <see cref="UdpChannel"/>
+    /// </summary>
+    public class UdpChannelSettings
+    {
+        /// <summary>
+        /// Statsd server host. Must be either valid ip address or dns name
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// Port a statsd server listens to
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// <see cref="UdpChannel"/> will throw error if DNS cannot be resolved.
+        /// </summary>
+        public bool IgnoreDnsErrors { get; set; }
+
+        /// <summary>
+        /// Validates settings and throws an exception in case of invalid settings
+        /// </summary>
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Host))
+            {
+                throw new ArgumentException($"Expected a valid host name, but received '{Host ?? "<null>"}'", nameof(Host));
+            }
+
+            if (Port <= 0 || UInt16.MaxValue < Port)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Port), $"Expected a number between 1 and {UInt16.MaxValue} but received {Port}");
+            }
+        }
+    }
+}
