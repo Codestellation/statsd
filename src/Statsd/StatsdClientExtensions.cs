a51af4d6c8fa90310f8dd54e1d27c1442069f197
Jury Soldatenkov <yurysold@gmail.com>
2017-03-31 18:46:25 +0300

    Added extension methods to simplify simple usages

---
+++ src/Statsd/StatsdClientExtensions.cs
@@ -0,0 +1,44 @@
+﻿namespace Codestellation.Statsd
+{
+    /// <summary>
+    /// Provides a few metrics to simplify metrics logging and enable log methods chaining
+    /// </summary>
+    public static class StatsdClientExtensions
+    {
+        /// <summary>
+        /// Sends count metric into a <see cref="IStatsdClient"/>
+        /// </summary>
+        /// <param name="self">An instance of the <see cref="IStatsdClient"/></param>
+        /// <param name="name">Name of a metric</param>
+        /// <param name="value">Number of times something happened</param>
+        public static IStatsdClient LogCount(this IStatsdClient self, string name, int value)
+        {
+            self.LogCount(new Count(name, value));
+            return self;
+        }
+
+        /// <summary>
+        /// Sends gauge metric into a <see cref="IStatsdClient"/>
+        /// </summary>
+        /// <param name="self">An instance of the <see cref="IStatsdClient"/></param>
+        /// <param name="name">Name of a metric</param>
+        /// <param name="value">Current level of something</param>
+        public static IStatsdClient LogGauge(this IStatsdClient self, string name, int value)
+        {
+            self.LogGauge(new Gauge(name, value));
+            return self;
+        }
+
+        /// <summary>
+        /// Sends gauge metric into a <see cref="IStatsdClient"/>
+        /// </summary>
+        /// <param name="self">An instance of the <see cref="IStatsdClient"/></param>
+        /// <param name="name">Name of a metric</param>
+        /// <param name="value">Time interval in milliseconds</param>
+        public static IStatsdClient LogTiming(this IStatsdClient self, string name, int value)
+        {
+            self.LogTiming(new Timing(name, value));
+            return self;
+        }
+    }
+}
