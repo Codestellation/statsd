134b8716037c54c5759590e97703e23d556a6554
Jury Soldatenkov <yurysold@gmail.com>
2017-05-23 21:38:48 +0300

    Improved performance of number serialization

--- src/Bench/StatsdWriterBenchmark.cs
+++ src/Bench/StatsdWriterBenchmark.cs
@@ -1,4 +1,5 @@
-﻿using BenchmarkDotNet.Attributes.Jobs;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
 using Codestellation.Statsd.Internals;
 
 namespace Codestellation.Statsd.Bench
@@ -13,6 +14,46 @@ namespace Codestellation.Statsd.Bench
         {
             _writer = new StatsdWriter("test");
             _metric = new Metric(new Timing("time", 814519243));
+        }
+
+        [Benchmark]
+        public void Write_metric()
+        {
+            const string metricName = "time";
+
+            _writer.WriteName(metricName);
+            _writer.WriteValue(1);
+            _writer.WritePostfix('m', 's');
+
+            _writer.WriteName(metricName);
+            _writer.WriteValue(12);
+            _writer.WritePostfix('m', 's');
+
+            _writer.WriteName(metricName);
+            _writer.WriteValue(123);
+            _writer.WritePostfix('m', 's');
+
+            _writer.WriteName(metricName);
+            _writer.WriteValue(1234);
+            _writer.WritePostfix('m', 's');
+
+            _writer.WriteName(metricName);
+            _writer.WriteValue(12345);
+            _writer.WritePostfix('m', 's');
+
+            _writer.WriteName(metricName);
+            _writer.WriteValue(123456);
+            _writer.WritePostfix('m', 's');
+
+            _writer.WriteName(metricName);
+            _writer.WriteValue(1234567);
+            _writer.WritePostfix('m', 's');
+
+            _writer.WriteName(metricName);
+            _writer.WriteValue(12345678);
+            _writer.WritePostfix('m', 's');
+
+            _writer.Reset();
         }
     }
 }
---
+++ src/Statsd/Internals/IntegerBuffer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Codestellation.Statsd.Internals
+{
+    internal unsafe struct IntegerBuffer
+    {
+        private const int DigitSymbolOffset = '0';
+        private const int BufferSize = 11;
+
+        ///Maximum integer number length in decimal representation is 10, plus a byte for a sign
+        private fixed byte _numberBuffer[BufferSize];
+
+        //The method is not supposed to change parameters, but position (ref is simple faster than normal parameter)
+        public void WriteNumber(ref byte[] buffer, ref int position, ref int value)
+        {
+            fixed (byte* nbp = _numberBuffer)
+            {
+                int numberBufferOffset = BufferSize;
+                for (int quotient = value; quotient != 0; quotient /= 10)
+                {
+                    nbp[--numberBufferOffset] = (byte)(DigitSymbolOffset + quotient % 10);
+                }
+
+                var length = BufferSize - numberBufferOffset;
+#if NETSTANDARD1_6 || NET46
+                fixed (byte* buf = &buffer[position])
+                {
+                    Buffer.MemoryCopy(nbp + numberBufferOffset, buf, Math.Min(buffer.Length - position, length), length);
+                }
+#else
+                for (int i = numberBufferOffset; i < numberBufferOffset + length; i++)
+                {
+                    buffer[position++] = nbp[i];
+                }
+
+#endif
+                position += length;
+            }
+        }
+    }
+}
