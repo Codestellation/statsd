737658eba9da02cb7eb5fe0a4d0241731ab9e6e3
Jury Soldatenkov <yurysold@gmail.com>
2017-05-31 01:07:09 +0300

    Created infrastructure for benchmarking

---
+++ src/Bench/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace Codestellation.Statsd.Bench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<StatsdWriterBenchmark>();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Bench/StatsdWriterBenchmark.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes.Jobs;
+using Codestellation.Statsd.Internals;
+
+namespace Codestellation.Statsd.Bench
+{
+    [ClrJob, CoreJob, LegacyJitX64Job, RyuJitX64Job]
+    public class StatsdWriterBenchmark
+    {
+        private readonly StatsdWriter _writer;
+        private Metric _metric;
+
+        public StatsdWriterBenchmark()
+        {
+            _writer = new StatsdWriter("test");
+            _metric = new Metric(new Timing("time", 814519243));
+        }
+    }
+}
