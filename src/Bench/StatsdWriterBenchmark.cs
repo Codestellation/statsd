106b2ac2beebfb5e797c6eb2a7f32d42584f1e37
Jury Soldatenkov <yurysold@gmail.com>
2017-06-03 00:35:11 +0300

    wip

--- src/Bench/StatsdWriterBenchmark.cs
+++ src/Bench/StatsdWriterBenchmark.cs
@@ -16,6 +16,8 @@ namespace Codestellation.Statsd.Bench
         private Metric _metric6;
         private Metric _metric7;
         private Metric _metric8;
+        private readonly byte[] _postfix;
+        private readonly int _intPostfix;
 
         public StatsdWriterBenchmark()
         {
@@ -28,95 +30,66 @@ namespace Codestellation.Statsd.Bench
             _metric6 = new Metric(new Timing("time", 123456));
             _metric7 = new Metric(new Timing("time", 1234567));
             _metric8 = new Metric(new Timing("time", 12345678));
+            _postfix = new[] { (byte)'|', (byte)'m', (byte)'s' };
+
+            _intPostfix = ((byte)'|') << 24 + ((byte)'m') << 16 + (byte)'s' << 8 + 3;
         }
 
         [Benchmark(Baseline = true)]
-        public void Write_metric_three_calls()
+        public void Postfix_aggressive_inlining()
         {
-            _writer.WriteName(_metric1.Name);
-            _writer.WriteValue(_metric1.Value);
             _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric2.Name);
-            _writer.WriteValue(_metric3.Value);
             _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric3.Name);
-            _writer.WriteValue(_metric4.Value);
             _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric4.Name);
-            _writer.WriteValue(_metric4.Value);
             _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric5.Name);
-            _writer.WriteValue(_metric5.Value);
             _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric6.Name);
-            _writer.WriteValue(_metric6.Value);
             _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric7.Name);
-            _writer.WriteValue(_metric7.Value);
             _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric8.Name);
-            _writer.WriteValue(_metric8.Value);
             _writer.WritePostfix('m', 's');
 
             _writer.Reset();
         }
 
         [Benchmark]
-        public void Write_metric_three_calls2()
+        public void Postfix_non_aggressive_inlining()
         {
-            _writer.WriteName(_metric1.Name);
-            _writer.WriteValue2(_metric1.Value);
-            _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric2.Name);
-            _writer.WriteValue2(_metric3.Value);
-            _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric3.Name);
-            _writer.WriteValue2(_metric4.Value);
-            _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric4.Name);
-            _writer.WriteValue2(_metric4.Value);
-            _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric5.Name);
-            _writer.WriteValue2(_metric5.Value);
-            _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric6.Name);
-            _writer.WriteValue2(_metric6.Value);
-            _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric7.Name);
-            _writer.WriteValue2(_metric7.Value);
-            _writer.WritePostfix('m', 's');
-
-            _writer.WriteName(_metric8.Name);
-            _writer.WriteValue2(_metric8.Value);
-            _writer.WritePostfix('m', 's');
+            _writer.WritePostfixPassive('m', 's');
+            _writer.WritePostfixPassive('m', 's');
+            _writer.WritePostfixPassive('m', 's');
+            _writer.WritePostfixPassive('m', 's');
+            _writer.WritePostfixPassive('m', 's');
+            _writer.WritePostfixPassive('m', 's');
+            _writer.WritePostfixPassive('m', 's');
+            _writer.WritePostfixPassive('m', 's');
 
             _writer.Reset();
         }
 
         [Benchmark]
-        public void Write_metric_by_ref()
+        public void Postfix_array_cast_pointers_from_int()
         {
-            _writer.Write(ref _metric1);
-            _writer.Write(ref _metric2);
-            _writer.Write(ref _metric3);
-            _writer.Write(ref _metric4);
-            _writer.Write(ref _metric5);
-            _writer.Write(ref _metric6);
-            _writer.Write(ref _metric7);
-            _writer.Write(ref _metric8);
+            _writer.WritePostfixArrayCastPointer(_intPostfix);
+            _writer.WritePostfixArrayCastPointer(_intPostfix);
+            _writer.WritePostfixArrayCastPointer(_intPostfix);
+            _writer.WritePostfixArrayCastPointer(_intPostfix);
+            _writer.WritePostfixArrayCastPointer(_intPostfix);
+            _writer.WritePostfixArrayCastPointer(_intPostfix);
+            _writer.WritePostfixArrayCastPointer(_intPostfix);
+            _writer.WritePostfixArrayCastPointer(_intPostfix);
+            _writer.Reset();
+        }
+
+        [Benchmark]
+        public void Postfix_aggressive_by_type()
+        {
+            _writer.WritePostfix(Type.Timing);
+            _writer.WritePostfix(Type.Timing);
+            _writer.WritePostfix(Type.Timing);
+            _writer.WritePostfix(Type.Timing);
+            _writer.WritePostfix(Type.Timing);
+            _writer.WritePostfix(Type.Timing);
+            _writer.WritePostfix(Type.Timing);
+            _writer.WritePostfix(Type.Timing);
 
             _writer.Reset();
         }
--- src/Statsd/Internals/StatsdWriter.cs
+++ src/Statsd/Internals/StatsdWriter.cs
@@ -7,37 +7,27 @@ namespace Codestellation.Statsd.Internals
 {
     internal class StatsdWriter
     {
-        private readonly string _prefix;
         private const int MaxMtu = 3 * 128;
         private static readonly Encoding Encoding = new UTF8Encoding(false);
 
-        private byte[] _buffer;
-        private int _position;
+        private readonly string _prefix;
         private readonly Dictionary<string, byte[]> _stringCache;
-        private IntegerBuffer _intBuffer;
-
-        public byte[] Buffer => _buffer;
-
-        public int Position => _position;
-
-        public bool MtuExceeded => _position >= MaxMtu;
-        public bool ContainsData => _position > 0;
+        private int _position;
+        private readonly byte[] _buffer;
 
         public StatsdWriter(string prefix)
         {
             _prefix = prefix;
-            _intBuffer = new IntegerBuffer();
             _buffer = new byte[1024];
             _position = 0;
             _stringCache = new Dictionary<string, byte[]>(StringComparer.Ordinal);
         }
 
-        public void Write(ref Metric metric)
-        {
-            WriteName(metric.Name);
-            WriteValue2(metric.Value);
-            metric.WritePostfix(ref _buffer, ref _position);
-        }
+        public byte[] Buffer => _buffer;
+        public int Position => _position;
+
+        public bool MtuExceeded => Position >= MaxMtu;
+        public bool ContainsData => Position > 0;
 
         public void WriteName(string name)
         {
@@ -48,7 +38,7 @@ namespace Codestellation.Statsd.Internals
                 utf8Array = Encoding.GetBytes(fullname);
                 _stringCache[name] = utf8Array;
             }
-            Array.Copy(utf8Array, 0, _buffer, _position, utf8Array.Length);
+            Array.Copy(utf8Array, 0, _buffer, Position, utf8Array.Length);
             _position += utf8Array.Length;
         }
 #if !NET40
@@ -108,17 +98,13 @@ namespace Codestellation.Statsd.Internals
                 length = 8;
             }
             else if (value < 1_000_000_000)
-            {
                 length = 9;
-            }
             else
-            {
                 length = 10;
-            }
 
-            for (int i = _position + length - 1; i >= _position; i--)
+            for (var i = Position + length - 1; i >= Position; --i)
             {
-                _buffer[i] = (byte)('0' + value % 10);
+                _buffer[i] = (byte)(offset + value % 10);
                 value /= 10;
             }
 
@@ -143,6 +129,13 @@ namespace Codestellation.Statsd.Internals
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
         public void WritePostfix(char c1, char c2)
+        {
+            _buffer[_position++] = (byte)'|';
+            _buffer[_position++] = (byte)c1;
+            _buffer[_position++] = (byte)c2;
+        }
+
+        public void WritePostfixPassive(char c1, char c2)
         {
             _buffer[_position++] = (byte)'|';
             _buffer[_position++] = (byte)c1;
@@ -174,6 +167,26 @@ namespace Codestellation.Statsd.Internals
         public void Reset()
         {
             _position = 0;
+        }
+
+        internal unsafe void WritePostfixArrayCastPointer(int intPostfix)
+        {
+            fixed (byte* d = &_buffer[_position])
+            {
+                switch (((byte*)&intPostfix)[0])
+                {
+                    case 2:
+                        d[1] = ((byte*)&intPostfix)[1];
+                        d[2] = ((byte*)&intPostfix)[2];
+                        break;
+                    case 3:
+                        d[1] = ((byte*)&intPostfix)[1];
+                        d[2] = ((byte*)&intPostfix)[2];
+                        d[3] = ((byte*)&intPostfix)[3];
+                        break;
+                }
+            }
+            _position += ((byte*)&intPostfix)[0];
         }
     }
 }
